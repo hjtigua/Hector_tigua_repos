ab2c7c93a3d0e7a5e4298aaf1566acb965eaae9e
hjtigua <tiguaguerrero@hotmail.com>
2022-10-24 20:45:43 -0500

    setup database infrastructure and orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+/*
+ TypeOrmModule.forRoot({
+      type: 'cockroachdb',
+      url: 'postgresql://hector:8umsjQTEjp1LzistQb3HoA@free-tier14.aws-us-east-1.cockroachlabs.cloud:26257/deunaDB?sslmode=verify-full',
+      ssl: true,
+      extra: {
+        options: '--cluster=cyan-ninja-6119',
+      },
+      logging: false,
+    }),
+*/
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '1234',
+      database: 'deunaDB',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
